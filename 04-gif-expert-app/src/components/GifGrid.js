b5e31910c9a91b0038549b7f994526215a3a775d
Magali Gracia <magali.gracia@neoris.com>
2022-05-26 12:59:22 -0300

    update

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,23 +1,19 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({category}) => {
-    const [images, setImagenes] = useState([]);
-
-    useEffect(() => {
-        getGifs(category)
-            .then(setImagenes);
-    }, [category]); 
+    const {data: images, loading} = useFetchGifs(category);
+ 
 
 
     return (
         <>
-            <h3>{category}</h3>
+            <h3 className = 'animate__animated animate__fadeIn'>{category}</h3>
+            {loading && <p  className = 'animate__animated animate__flash'>Loading...</p>}
             <div className='card-grid'>
                 <ol>
                     {
-                        images.map(img => (
+                        images.map(img => ( // este images es el alias de data
                             <GifGridItem 
                             key = {img.id}
                             {...img}/>
